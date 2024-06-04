b8d32e43eb96319a24543103d813be59d6fd3e62
aortac82 <158775703+aortac82@users.noreply.github.com>
2024-06-04 14:45:21 +0300

    v11

--- src/test/java/team03_AlloverCommerceTestNG/pages/P7_ShoppingPage.java
+++ src/test/java/team03_AlloverCommerceTestNG/pages/P7_ShoppingPage.java
@@ -12,7 +12,6 @@ public class P7_ShoppingPage {
     }
 
 
-
     @FindBy(xpath = "//input[@class='form-control']")
     public WebElement searchBox;
 
@@ -21,6 +20,9 @@ public class P7_ShoppingPage {
 
     @FindBy(xpath = "//*[@id=\"header\"]/div/div/div/div/div/div[6]/div/div")
     public WebElement cart;
+
+    @FindBy(xpath = "///button[@name='update_cart']")
+    public WebElement updateCart;
 
     @FindBy(xpath = "//*[@id=\"header\"]/div/div/div/div/div/div[6]/div/div/div[2]/div/p[2]/a[1]")
     public WebElement viewcart;
@@ -31,14 +33,20 @@ public class P7_ShoppingPage {
     @FindBy(xpath = "//button[@title='Minus']")
     public WebElement buttonMinus;
 
+    @FindBy(xpath = "//input[@type='number']")
+    public WebElement inputnumber;
+
     @FindBy(xpath = "/html/body/div[1]/div[1]/div/ul/li[2]/a")
     public WebElement billing;
 
-    @FindBy(xpath = "//input[@value='cod']")
-    public WebElement paydoor;
+    @FindBy(id = "//input[@name='ship_to_different_address']")
+    public WebElement differentAddress;
 
     @FindBy(xpath = "//input[@value='bacs']")
-    public WebElement eftwire;
+    public WebElement eftWire;
+
+    @FindBy(xpath = "//input[@value='cod']")
+    public WebElement paydoor;
 
     @FindBy(css = "#place_order")
     public WebElement placeorder;
@@ -49,8 +57,24 @@ public class P7_ShoppingPage {
     @FindBy(xpath = "//button[@name='apply_coupon']")
     public WebElement applyCoupon;
 
-    @FindBy(xpath = "//button[@name='clear_cart']")
-    public WebElement clearcart;
+    @FindBy(xpath = "//*[@id=\"main\"]/div/div/div/div/div/div[2]/div[1]/form/table/tbody/tr[1]/td[1]/a[2]")    ////button[@name='clear_cart']
+    public WebElement clearCart;
+
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div/ul/li[2]/a")
+    public WebElement checkout;
+
+    @FindBy(xpath = "//*[@id=\"header\"]/div/div/div/div/div/div[9]/div/div/a/span")
+    public WebElement signout;
+
+    @FindBy(xpath = "//*[.='Thank you. Your order has been received.']")
+    public WebElement orderComplete;
+
+    @FindBy(xpath = "//div[@class='woocommerce-order']")
+    public WebElement endOrder;
+
+    @FindBy(xpath = "//*[@id=\"order_review\"]/table/tfoot/tr[1]")
+    public WebElement subTotal;
+
 
 
 
